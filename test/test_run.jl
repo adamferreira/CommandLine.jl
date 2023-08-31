34800d9781fb21bf1b0d6a4b5aa8f1ae7635b309
adamferreira <adam.ferreira.dc@gmail.com>
2023-08-31 22:38:36 +0200

    Testing Shell environnement variables

--- test/test_run.jl
+++ test/test_run.jl
@@ -10,3 +10,12 @@
     outputs = checkoutput(s, `"for((i=1;i<=3;i+=1)); do sleep 0; echo "Test"; done"`)
     @test outputs == ["Test", "Test", "Test"]
 end
+
+@testset "Test env variables" begin
+    s = LocalGitBash(; pwd = "~", env = Dict("Toto" => "Tata"))
+    @test s["Toto"] == s[:Toto] == "Tata"
+    s["Toto"] = "Titi"
+    @test s["Toto"] == s[:Toto] == "Titi"
+    s[:Toto] = :Tutu
+    @test s["Toto"] == s[:Toto] == "Tutu"
+end
